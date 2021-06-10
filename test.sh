25e3baec7f83447921462de80e65eb8e90836cc9
notdansls <50490663+notdansls@users.noreply.github.com>
2021-06-10 21:57:17 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,9 +2,8 @@ echo "Test is sucessful!"
 echo "since you where able to clone this repository, we'll assume you have a stable internet connection"
 timedatectl set-ntp true
 echo "lets get the hdd, and dump it into a variable"
-STORAGE=$(lsblk | grep disk | cut -c1-3)
+STORAGE="/dev/$(lsblk | grep disk | cut -c1-3)"
 echo "Warning, this process will wipe the disk! HAHA"
 echo "Grabbing the HDD device..."
-export STORAGE="/dev/${lsblk | grep disk | cut -c1-3}"
-echo "Targeting " $HDD
+echo "Targeting " $STORAGE
 #(&>/dev/null script.sh &)
