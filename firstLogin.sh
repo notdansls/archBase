e4080f90ea0b973fd70835c49ff8d9ace8056b7e
notdansls <50490663+notdansls@users.noreply.github.com>
2021-06-10 22:43:24 +0100

    Update firstLogin.sh

--- firstLogin.sh
+++ firstLogin.sh
@@ -1,1 +1,4 @@
 echo "Dunno if this will work but gotta be worth a try..."
+ln -sf /usr/share/zoneinfo/Europe/London /etc/localtime
+hwclock --systohc
+# the next stage involves opening /etc/locale.gen and uncommenting en_GB.UTF8 UTF8 and running locale-gen.
