b46c00df3bf63da657b1583f179ab87969758019
notdansls <50490663+notdansls@users.noreply.github.com>
2021-06-10 22:16:59 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -12,3 +12,6 @@ PARTITION="$STORAGE$PARTNO"
 # echo $PARTITION
 mkfs.ext4 $PARTITION
 mount $PARTITION /mnt
+# Now we're doing the interesting stuff, we're going to pacstrap the installation.
+# Packages at present - base linux linux-firmware base-devel git vim NetworkManager openssh grub
+pacstrap /mnt base linux linux-firmware base-devel git vim NetworkManager openssh grub
