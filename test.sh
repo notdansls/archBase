4028a1daf86c433406d895b55fce46c16bc94ba5
notdansls <50490663+notdansls@users.noreply.github.com>
2021-06-10 21:55:03 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,6 +5,6 @@ echo "lets get the hdd, and dump it into a variable"
 STORAGE=$(lsblk | grep disk | cut -c1-3)
 echo "Warning, this process will wipe the disk! HAHA"
 echo "Grabbing the HDD device..."
-export STORAGE=/dev/$(lsblk | grep disk | cut -c1-3)
+export STORAGE="/dev/$(lsblk | grep disk | cut -c1-3)"
 echo "Targeting " $HDD
 #(&>/dev/null script.sh &)
