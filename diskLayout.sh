802485eb320ba3ed4d44d168f680bb4facfee3f6
notdansls <50490663+notdansls@users.noreply.github.com>
2021-06-10 21:02:10 +0100

    Update diskLayout.sh
    Modified to suit my configuration.

--- diskLayout.sh
+++ diskLayout.sh
@@ -7,20 +7,17 @@
 # Note that a blank line (commented as "defualt" will send a empty
 # line terminated with a newline to take the fdisk default.
 sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk ${TGTDEV}
-  o # clear the in memory partition table
+  g # Create a new empty GPT partition table
   n # new partition
-  p # primary partition
-  1 # partition number 1
+    # default - partition 1
     # default - start at beginning of disk 
-  +1M # 100 MB boot parttion
+  1M # 1M boot partition
+  t # Change the partition type
+  4 # Change the type to BIOS Boot
   n # new partition
   p # primary partition
-  2 # partion number 2
-    # default, start immediately after preceding partition
-    # default, extend partition to end of disk
-  a # make a partition bootable
-  1 # bootable partition is partition 1 -- /dev/sda1
-  p # print the in-memory partition table
+    # default - partition 2
+    # default - first free sector
+    # default - extend partition to end of disk
   w # write the partition table
-  q # and we're done
 EOF
