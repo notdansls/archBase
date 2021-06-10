4c4cde0da898b94d2f5c07569e13ce006969f297
notdansls <50490663+notdansls@users.noreply.github.com>
2021-06-10 21:51:44 +0100

    Update test.sh
    first steps

--- test.sh
+++ test.sh
@@ -1,1 +1,10 @@
-echo "Test is sucessfull!"
+echo "Test is sucessful!"
+echo "since you where able to clone this repository, we'll assume you have a stable internet connection"
+timedatectl set-ntp true
+fdisk -l 
+echo "lets get the hdd, and dump it into a variable"
+STORAGE=$(lsblk | grep disk | cut -c1-3)
+echo "Warning, this process will wipe the disk! HAHA"
+echo "Grabbing the HDD device..."
+export STORAGE=/dev/$(lsblk | grep disk | cut -c1-3)
+#(&>/dev/null script.sh &)
