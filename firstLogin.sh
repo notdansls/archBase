7b3f2b3d48ad4626b7b9c483457fcbc7972f58b0
notdansls <50490663+notdansls@users.noreply.github.com>
2021-06-11 09:22:49 +0100

    Update firstLogin.sh
    multiple changes, created sed entry to un-comment en_GB languages and generate locales. added en_GB.UTF-8 to /etc/locale.conf. Added archbase to hostnames and created hosts file. requested password change. Next up is to get grub working.

--- firstLogin.sh
+++ firstLogin.sh
@@ -2,3 +2,13 @@ echo "Dunno if this will work but gotta be worth a try..."
 ln -sf /usr/share/zoneinfo/Europe/London /etc/localtime
 hwclock --systohc
 # the next stage involves opening /etc/locale.gen and uncommenting en_GB.UTF8 UTF8 and running locale-gen.
+# Uncomment en_GB and confirm...
+echo "| Configuring Languages..."
+sed -i.bak '/en_GB/s/^#//g' /etc/locale.gen
+locale-gen
+echo "LANG=en_GB.UTF-8" > /etc/locale.conf
+echo "| Configuring Networking..."
+echo "archbase" > /etc/hostname
+printf "127.0.0.1\tlocalhost\n::1\t\tlocalhost\n127.0.1.1\tarchbase.localdomain archbase\n" >> /etc/hosts
+passwd 
+exit
