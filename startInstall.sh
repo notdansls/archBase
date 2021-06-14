a3e6957413be069e7ffa5ca397f4a790248017f8
notdansls <50490663+notdansls@users.noreply.github.com>
2021-06-14 21:25:17 +0100

    re-write of startInstall.sh
    Split functions out, improved user interaction, added some functionality.

--- startInstall.sh
+++ startInstall.sh
@@ -1,30 +1,77 @@
-echo "Test is sucessful!"
-echo "since you where able to clone this repository, we'll assume you have a stable internet connection"
-timedatectl set-ntp true
-echo "lets get the hdd, and dump it into a variable"
-STORAGE="/dev/$(lsblk | grep disk | cut -c1-3)"
-echo "Warning, this process will wipe the disk! HAHA"
-echo "Grabbing the HDD device..."
-echo "Targeting " $STORAGE
-(&>/dev/null . ~/archBase/diskLayout.sh &)
-PARTNO=2
-PARTITION="$STORAGE$PARTNO"
-# echo $PARTITION
-mkfs.ext4 $PARTITION -F
-mount $PARTITION /mnt
-# Now we're doing the interesting stuff, we're going to pacstrap the installation.
-# Packages at present - base linux linux-firmware base-devel git vim NetworkManager openssh grub
-pacstrap /mnt base linux linux-firmware base-devel git vim networkmanager openssh grub
-# short version just to get up and running quickly...
-#pacstrap /mnt base linux linux-firmware grub
-# Next we'll generate the fstab file
-genfstab -U /mnt >> /mnt/etc/fstab
-# Copy archBase to /mnt/root/...
-cp -r /root/archBase /mnt/root/
-# Now we want to change root into the new environment
-echo ". ~/archBase/firstLogin.sh" >> /mnt/root/.bashrc
-arch-chroot /mnt
-# From where, I don't quite now how we'll automate it, but I'll work it out. come back for more soon...
-echo "| At this point we're done. We're going to unmount everything and go for a reboot..."
-umount -R /mnt
-reboot
+function confirmProcedure {
+	clear
+	echo
+	echo "|"
+	echo "| ! !                   W A R N I N G                   ! ! "
+	echo "|"
+	echo "| ! !     D I S K   E R A S U R E   I M M I N E N T     ! ! "
+	echo "|"
+	read -p '| To proceed, please type proceed: ' procedureConfirmed
+	if [ $procedureConfirmed != "proceed" ]
+	then
+		exit 22 
+	fi	
+}
+
+function getInputs {
+	clear
+	echo
+	echo "|"
+	echo "| To ensure that this process runs smoothly, we want to "
+	echo "| gather some information now to ensure effecient processing"
+	echo "|"
+	echo "| This will ensure no further prompts after this"
+	echo "|"
+	read -p  '| User Name: ' userID
+	read -sp '| Password:  ' userPW
+	read -p	 '| hostname:  ' hostName
+}
+
+function writeOutputs {
+	echo "stdUserID=$userID" >> ~/archBase.conf
+	echo "stdUserPW=$userPW" >> ~/archBase.conf
+	echo "aHostname=$hostName" >> ~/archBase.conf
+	echo "Username, Password, and hostname  written to file..."
+	unset userID
+	unset userPW
+	unset hostName
+}
+
+function prepareEnvironment {
+	timedatectl set-ntp true
+	storage="/dev/$(lsblk | grep disk | heand -n 1 | cut -c1-3)"
+	(&/dev/null . ~/archBase/diskLayout.sh &)
+	partition=$storage"2"
+	mkfs.ext4 $partition -F
+	mount $partition /mnt
+#	pacstrap /mnt base linux linux-firmware base-devel git vim networkmanager openssh grub sudo
+	pacstrap /mnt base linux linux-firmware vim networkmanager grub sudo
+	genfstap -U /mnt >> /mnt/etc/fstab
+	cp -r /root/archBase /mnt/root/
+	echo ". ~/archBase/firstLogin.sh" >> /mnt/root/.bashrc
+	arch-chroot /mnt
+}
+
+function readConfig {
+	arbid=$(cat ~/archBase.conf | grep stdUserID | cut -c 11-32)
+	arbpw=$(cat ~/archBase.conf | grep stdUserPW | cut -c 11-32)
+	echo "We have read the user '$arbid' from the config file."
+}
+
+function cleanup {
+	clean
+	umount -R /mnt
+	echo "|"
+	echo "| The configuration is now complete, rebooting now..."
+	echo "|"
+	reboot
+}
+
+confirmProcedure	# Confirm procedure will run to ensure the user wants to 
+			# erase the disk before continuing the process. if any
+			# input other than proceed is received it will bail,
+			# nothing else will run.
+getInputs		# Get the inputs from the user
+writeOutputs		# write the inputs gathered to the archBase.conf file
+prepareEnvironment	# Get the environemnt ready for first boot
+cleanup			# Unmount everything and reboot everything
