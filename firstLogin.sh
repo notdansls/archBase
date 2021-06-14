eadf2f7278387fa2ae19e45b76431cde44a6286f
notdansls <50490663+notdansls@users.noreply.github.com>
2021-06-14 21:21:39 +0100

    complete rewrite of firstLogin.sh
    Split actions into functions, included more detail in configuration.

--- firstLogin.sh
+++ firstLogin.sh
@@ -1,25 +1,52 @@
-echo "Dunno if this will work but gotta be worth a try..."
-ln -sf /usr/share/zoneinfo/Europe/London /etc/localtime
-hwclock --systohc
-# the next stage involves opening /etc/locale.gen and uncommenting en_GB.UTF8 UTF8 and running locale-gen.
-# Uncomment en_GB and confirm...
-echo "| Configuring Languages..."
-sed -i.bak '/en_GB/s/^#//g' /etc/locale.gen
-locale-gen
-echo "LANG=en_GB.UTF-8" > /etc/locale.conf
-echo "| Configuring Networking..."
-echo "archbase" > /etc/hostname
-printf "127.0.0.1\tlocalhost\n::1\t\tlocalhost\n127.0.1.1\tarchbase.localdomain archbase\n" >> /etc/hosts
-echo "| You'll need to set a root password here."
-echo "| you will need the password once we have restarted."
-passwd
-# need to do grub here...
-echo "| Configuring GRUB Bootloader..."
-DISK="/dev/$(lsblk | grep disk | grep G | cut -c1-3)"
-grub-install --target=i386-pc $DISK
-grub-mkconfig -o /boot/grub/grub.cfg
-# need to remove line from .bashrc to prevent reload
-sed -i.bak '/firstLogin/d' ~/.bashrc
-echo "I think we're done..."
-echo "                     ...for now..."
-exit
+function configureTimeZone {
+	ln -sf /user/share/zoneinfo/Europe/London /etc/localtime
+	hwclock --systohc
+}
+
+function configureRegion {
+	loadkeys uk
+	sed -i.bak '/en_GB/s/^#//g' /etc/locale.gen
+	locale-gen
+	echo "LANG=en_GB.UTF-8" > /etc/locale.conf
+}
+
+function configureNetworking {
+	echo $arbhn >> /etc/hostname
+	printf "127.0.0.1\tlocalhost\n::1\t\tlocalhost\n127.0.1.1\t$arbhn.localdomain $arbhn\n" >> /etc/hosts
+}
+
+function readConfig {
+	arbid=$(cat ~/archBase.conf | grep stdUserID | cut -c 11-32)
+	arbpw=$(cat ~/archBase.conf | grep stdUserPW | cut -c 11-32)
+	arbhn=$(cat ~/archBase.conf | grep aHostname | cut -c 11-32)
+	echo "We have read the user '$arbid' from the config file."
+}
+
+function createStdUser {
+	useradd -G wheel $arbid
+	echo -e "$arbpw\n$arbpw" | sudo passwd $arbid
+	sed -i.bak '/%wheel ALL=(ALL) ALL/s/^#//g' /etc/sudoers
+}
+
+function resetRoot {
+	rootPW=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 64 | head -n 1)
+	echo root:$rootPW | chpasswd
+}
+
+function installBootloader {
+	disk="/dev/$(lsblk | grep disk | grep G | head -n 1 | cut -c1-3)"
+	grub-install --target=i386-pc $disk
+	grub-mkconfig -o /boot/grub/grub.cfg
+}
+
+function cleanupChroot {
+	sed -i.bak '/firstLogin/d' ~/.bashrc
+}
+
+readConfig		# Read the inputs from the previous scripts
+configureRegion		# Function does exactly as it sounds
+configureNetworking	# Function does exactly as it sounds
+createStdUser		# Creates new user within the new environment, give it sudo prives.
+resetRoot		# Resets root password to a random unknown string. Root will not be used again.
+installBootloader	# install the bootloader of choice
+cleanupChroot
